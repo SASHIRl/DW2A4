66e20122fd88ef26360f918a9dc762903ccd72b9
Diego Marques <marquesashiri@gmail.com>
2022-06-05 18:48:02 -0300

    Arquivo Widget.tsx criado, implementação da interface de acessibilidade Popover ee Phosphor

--- Atividades/projeto-spa/web/src/App.tsx
+++ Atividades/projeto-spa/web/src/App.tsx
@@ -1,20 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
-  return <button className=" bg-red-600 p-4 h-10 rounded text-black hover:bg-red-700 transition-colors">{props.text ?? 'Default'}</button>
+export function App() {
+  return <Widget />
 }
-
-function App() {
-  return  (
-    <div className="flex gap-2">
-      <Button text="O Botão"/>
-      <Button text="O outro botão"/>
-      <Button text="Mais um botão"/>
-      <Button text="Mestre da web"/>
-    </div>
-  )
-}
-
-export default App
