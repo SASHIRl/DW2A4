83fd7122e76ec86609b22d26e4439ff004db96e0
Diego Marques <marquesashiri@gmail.com>
2022-06-17 02:04:01 -0300

    Adiciona Tips para dicas de pixel art

--- Atividades/projeto-spa/web/src/main.tsx
+++ Atividades/projeto-spa/web/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
+import {Tips} from './Tips'
 
 import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Tips />
   </React.StrictMode>
 )
